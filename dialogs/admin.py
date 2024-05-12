ddbb5d4155336107a0a527e737e9c311e0d92a73
Dmitriy Fishman <fishman.code@gmail.com>
2024-05-12 18:53:42 +0300

    Updated admin query list

--- dialogs/admin.py
+++ dialogs/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Dialog, Query
 
-admin.site.register([Dialog, Query])
+admin.site.register([Dialog])
+
+
+@admin.register(Query)
+class QueryAdmin(admin.ModelAdmin):
+    list_display = (
+        "req",
+        "dialog",
+    )
